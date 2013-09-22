96c344694f514a73693da3bb7320e62675164c0d
Jamie Brewer <jamie.brwr@gmail.com>
2013-09-22 16:12:13 -0500

    Updating single-product/short-description
    Added link for contacting Author.

--- wp-content/themes/swap-and-shop/woocommerce/single-product/short-description.php
+++ wp-content/themes/swap-and-shop/woocommerce/single-product/short-description.php
@@ -21,27 +21,16 @@ if ( ! $post->post_excerpt ) return;
 global $current_user;
       get_currentuserinfo();
 
-		if ( is_user_logged_in() ) {
+		if ( is_user_logged_in() ) { ?>
 		   
 		   
-		   //echo '<strong>Username:</strong> ' . $current_user->user_login . "<br />";
-	      echo '<a class="button" href="mailto:' . $current_user->user_email . '">Contact Seller</a>';
-	      //echo '<strong>User first name:</strong> ' . $current_user->user_firstname . "<br />";
-	      //echo '<strong>User last name:</strong> ' . $current_user->user_lastname . "<br />";
-	      //echo '<strong>User display name:</strong> ' . $current_user->display_name . "<br />";
-	      //echo '<strong>User ID:</strong> ' . $current_user->ID . "<br />";
-	      echo '<p>&nbsp;</p>';
+		   <p><a class="button" href="mailto:<?php echo get_the_author_meta('user_email', $post->post_author); ?>">Contact Seller</a></p>
 		   
 		   
-		} else {
-		
-		//echo '<strong>Username:</strong> ' . $current_user->user_login . "<br />";
-	      echo 'You must '.wp_register( '<span class="button">', '<span>', false).' to contact the seller.';
-	      //echo '<strong>User first name:</strong> ' . $current_user->user_firstname . "<br />";
-	      //echo '<strong>User last name:</strong> ' . $current_user->user_lastname . "<br />";
-	      //echo '<strong>User display name:</strong> ' . $current_user->display_name . "<br />";
-	      //echo '<strong>User ID:</strong> ' . $current_user->ID . "<br />";
-	      echo '<p>&nbsp;</p>';
+		<?php } else {
+			
+			echo '<em><strong>You must '.wp_register( '<span class="button">', '<span>', false).' to contact the seller.</strong></em>';
+			echo '<p>&nbsp;</p>';
 
 		}
 		
