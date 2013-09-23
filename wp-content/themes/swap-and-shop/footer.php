58c1d29248ceac8afc7d6dd4be09138f9417cc83
Jamie Brewer <jamie.brwr@gmail.com>
2013-09-22 21:51:28 -0500

    Adding Stats to /todays-new-items/ Footer

--- wp-content/themes/swap-and-shop/footer.php
+++ wp-content/themes/swap-and-shop/footer.php
@@ -20,11 +20,15 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 		   woo_active_sidebar( 'footer-3' ) ||
 		   woo_active_sidebar( 'footer-4' ) ) && $total > 0 ) {
 
-?>	
+?>
+	
 	<?php woo_footer_before(); ?>
 		
 	<section id="footer-widgets" class="col-full col-<?php echo $total; ?> fix">
-
+	
+	
+	
+	
 		<h2> Awesome Website Stats</h2>
 		<?php
 		$result = count_users();
@@ -33,36 +37,30 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 		$numposts = $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->posts WHERE post_type = 'product'");
 		if (0 < $numposts) echo '<h3>There have been a total of <a href="/The-Market/market/"><span style="color: #2293e2;">'.$numposts = number_format($numposts).'</span></a> items listed to date.</h3>';
 		
-		
-		
-		
 		/* Count the amount of products added each day */
-		function filter_where($where = '') {
-		    //posts in the last 30 days
-		    $where .= " AND post_date > '" . date('Y-m-d', strtotime('-1 days')) . "'";
-		    return $where;
-		  }
-		add_filter('posts_where', 'filter_where');
-		    $args=array(
-		      'post_type' => 'product',
-		      'post_status' => 'publish',
-		      'showposts' => -1,
-		      'caller_get_posts'=> 1
-		      );
-		$my_query=new WP_Query($args);
-		remove_filter('posts_where', 'filter_where');
-		
-		  if( $my_query->have_posts() ) {
-		    echo '<h3>There have been <span style="color: #2293e2;">'.count($my_query->posts) . '</span> items added today.</h3>';
-		    while ($my_query->have_posts()) : $my_query->the_post(); ?>
-		      <p><small><?php the_time('m.d.y') ?></small> <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
-		     <?php
-		    endwhile;
-		  } //if ($my_query)
-		wp_reset_query(); //just in case
+		  function filter_where_footer($where = '') {
+			    //posts in the last 30 days
+			    $where .= " AND post_date > '" . date('Y-m-d', strtotime('-1 days')) . "'";
+			    return $where;
+			  }
+			add_filter('posts_where', 'filter_where_footer');
+			    $args=array(
+			      'post_type' => 'product',
+			      'post_status' => 'publish',
+			      'showposts' => -1,
+			      'caller_get_posts'=> 1
+			      );
+			$my_query2=new WP_Query($args);
+			remove_filter('posts_where', 'filter_where_footer');
+			
+			  if( $my_query2->have_posts() ) {
+			    echo '<h3>There have been <a href="/The-Market/todays-new-items/"><span style="color: #2293e2;">'.count($my_query2->posts) . '</span></a> items added to the site today!</h3>';
+			    while ($my_query2->have_posts()) : $my_query2->the_post();
+			    endwhile;
+			  } //if ($my_query)
+			wp_reset_query(); //just in case
+			?>
 
-
-		?>
 
 
 
@@ -102,8 +100,32 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 	</footer><!-- /#footer  -->
 
+
+
+
+
+
+
+
+
+
+
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 </div><!-- /#wrapper -->
 <?php wp_footer(); ?>
 <?php woo_foot(); ?>
 </body>
-</html>
+</html
