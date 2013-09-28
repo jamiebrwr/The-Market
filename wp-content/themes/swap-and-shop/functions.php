f0b99f2979038405e8736d87fa94ef1d17d4405b
Jamie Brewer <jamie.brwr@gmail.com>
2013-09-28 16:19:59 -0500

    Functions Cleanup
    Cleaned up the syntax and added phpDoc Blocks to the functions. Also
    removed Gravity Forms functions and Pippins Plugins frontend form
    functions. Replaced with Frontend User.

--- wp-content/themes/swap-and-shop/functions.php
+++ wp-content/themes/swap-and-shop/functions.php
@@ -1,320 +1,193 @@
 <?php
 
 /*-----------------------------------------------------------------------------------*/
-/* Start WooThemes Functions - Please refrain from editing this section */
+/* Start Functions - Please refrain from editing this section */
 /*-----------------------------------------------------------------------------------*/
 
 // Set path to WooFramework and theme specific functions
 $functions_path = STYLESHEETPATH . '/functions/';
 $includes_path = STYLESHEETPATH . '/includes/';
 
-// WooFramework
-//require_once ($functions_path . 'admin-init.php');			// Framework Init
-
 // Theme specific functionality
-//require_once ($includes_path . 'theme-options.php'); 			// Options panel settings and custom settings
 require_once ($includes_path . 'image-sizes.php'); 				// Register custom image sizes
-//require_once ($includes_path . 'theme-functions.php'); 		// Custom theme functions
-//require_once ($includes_path . 'theme-plugins.php');			// Theme specific plugins integrated in a theme
-//require_once ($includes_path . 'theme-actions.php');			// Theme actions & user defined hooks
-//require_once ($includes_path . 'theme-comments.php'); 		// Custom comments/pingback loop
-//require_once ($includes_path . 'theme-js.php');				// Load javascript in wp_head
-//require_once ($includes_path . 'sidebar-init.php');			// Initialize widgetized areas
-//require_once ($includes_path . 'theme-widgets.php');			// Theme widgets
 
 /*-----------------------------------------------------------------------------------*/
 /* You can add custom functions below */
 /*-----------------------------------------------------------------------------------*/
 
-function pippin_create_post_form() {
-	ob_start(); 
-	if(isset($_GET['post'])) {
-		switch($_GET['post']) {
-			case 'successfull':
-				echo '<p class="success">' . __('Stats Below', 'pippin') . '</p>';
-				break;
-			case 'failed' :
-				echo '<p class="error">' . __('Please fill in all the info', 'pippin') . '</p>';
-				break;
-		}
-	}
-	?>
-	<form id="pippin_create_post" action="" method="POST">
-		<fieldset>
-			<input name="website_url" id="website_url" type="text"/>
-			<label for="website_url">Website URL</label>
-		</fieldset>
-		<fieldset>
-			<?php wp_nonce_field('website_url_nonce', 'website_url_nonce_field'); ?>
-			<input type="submit" name="website_url_submit" value="<?php _e('Submit URL', 'pippin'); ?>"/>
-		</fieldset>
-	</form>
-	<?php 
-	return ob_get_clean();
-}
-add_shortcode('post_form', 'pippin_create_post_form');
- 
-function pippin_process_post_creation() {
-	if(isset($_POST['website_url_nonce_field']) && wp_verify_nonce($_POST['website_url_nonce_field'], 'website_url_nonce')) {
- 
-		if(strlen(trim($_POST['website_url'])) < 1 ) {
-			$redirect = add_query_arg('post', 'failed', $_POST['_wp_http_referer']);
-		} else {		
-			$website_url_info = array(
-				'post_title' => esc_attr(strip_tags($_POST['website_url_title'])),
-				'post_type' => 'website_urls',
-				'post_content' => esc_attr(strip_tags($_POST['website_url_desc'])),
-				'post_status' => 'pending'
-			);
-			$website_url_id = wp_insert_post($website_url_info);
- 
-			if($website_url_id) {
-				update_post_meta($website_url_id, 'ecpt_postedby', esc_attr(strip_tags($_POST['user_name'])));
-				update_post_meta($website_url_id, 'ecpt_posteremail', esc_attr(strip_tags($_POST['user_email'])));
-				update_post_meta($website_url_id, 'ecpt_contactemail', esc_attr(strip_tags($_POST['inquiry_email'])));
-				$redirect = add_query_arg('post', 'successfull', $_POST['_wp_http_referer']);
-			}
-		}
-		wp_redirect($redirect); exit;
-	}
-}
-add_action('init', 'pippin_process_post_creation');
 
-
-
-
-
-add_action("gform_field_advanced_settings", "ounce_gform_field_advanced_settings", 10, 2);
-function ounce_gform_field_advanced_settings($position, $form_id){
-    
-    if($position == 50){
-        ?>
-        <li id="populate_taxonomy_settings" style="display:block;">
-            <label for="field_admin_label">
-                <?php _e("Populate with a Taxonomy", "gravityforms"); ?>
-                <?php gform_tooltip("form_field_custom_taxonomy") ?>
-            </label>
-            <input type="checkbox" id="field_enable_populate_taxonomy" onclick="togglePopulateTaxonomy(jQuery('#field_populate_taxonomy'), '');" /> Enable population with a taxonomy<br />
-            
-            <select id="field_populate_taxonomy" onchange="SetFieldProperty('populateTaxonomy', jQuery(this).val());" style="margin-top:10px; display:none;">
-                <option value="" style="color:#999;">Select a Taxonomy</option>
-            <?php
-            $taxonomies = get_taxonomies('', 'objects');
-            foreach($taxonomies as $taxonomy): ?>
-            
-                <option value="<?php echo $taxonomy->name; ?>"><?php echo $taxonomy->label; ?></option>
-                
-            <?php endforeach; ?>
-            </select>
-            
-        </li>
-        <?php
-    }
-    
+/**
+ * Proper way to enqueue scripts and styles
+ */
+function theme_name_scripts() {
+	//wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+	//wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 }
 
-// action to inject supporting script to the form editor page
-add_action("gform_editor_js", "ounce_gform_editor_scripts");
-function ounce_gform_editor_scripts(){
-    ?>
-    <script type='text/javascript'>
+add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
 
-        jQuery(document).bind("gform_load_field_settings", function(event, field, form){
-            
-            var valid_types = new Array('select');
-            if(jQuery.inArray(field['type'], valid_types) != -1) {
-                jQuery('#populate_taxonomy_settings').show();
-            } else {
-                jQuery('#populate_taxonomy_settings').hide();
-            }
-            
-            var populateTaxonomy = (typeof field['populateTaxonomy'] != 'undefined' && field['populateTaxonomy'] != '') ? field['populateTaxonomy'] : false;
-            
-            jQuery("#field_enable_populate_taxonomy").attr("checked", populateTaxonomy != false);
-            togglePopulateTaxonomy(jQuery('#field_populate_taxonomy'), populateTaxonomy);
-            
-        });
-        
-        function togglePopulateTaxonomy(elem, taxonomy){
-            
-            var checked = jQuery("#field_enable_populate_taxonomy").attr('checked');
-            
-            if(checked){
-                jQuery(elem).slideDown(function(){
-                    jQuery(this).val(taxonomy); 
-                });
-            } else {
-                jQuery(elem).slideUp(function(){
-                    jQuery(this).val(taxonomy); 
-                });
-            }
-            
-            
-        }
-        
-    </script>
-    <?php
-}
 
-// filter to add a new tooltip
-add_filter('gform_tooltips', 'ounce_gform_tooltips');
-function ounce_gform_tooltips($tooltips){
-   $tooltips["form_field_custom_taxonomy"] = "<h6>Populate with a Taxonomy</h6>Check this box to populate this field from a taxonomy.";
-   return $tooltips;
-}
-
-// filter to populate taxonomy in designated fields
-add_filter('gform_pre_render', 'ounce_gform_populate_taxonomy');
-function ounce_gform_populate_taxonomy($form){
-    
-    foreach($form['fields'] as &$field){
-        
-        if(!$field['populateTaxonomy'])
-            continue;
-        
-        $taxonomy = $field['populateTaxonomy'];
-        $first_choice = $field['choices'][0]['text'];
-        $field['choices'] = ounce_taxonomy_as_choices($taxonomy, $first_choice);
-    }
-
-    return $form;
-}
-
-function ounce_taxonomy_as_choices($taxonomy = "categories", $first_choice = '') {
-    
-    $terms = get_terms($taxonomy, 'orderby=name&hide_empty=0');
-    $taxonomy = get_taxonomy($taxonomy);
-    $choices = array();
-    $i = 0;
-    
-    switch($first_choice){
-    
-    // if no default option is specified, dynamically create based on taxonomy name
-    case '':
-        $choices[$i]['text'] = "Select a {$taxonomy->labels->singular_name}";
-        $choices[$i]['value'] = "";
-        $i++;
-        break;
-        
-    // populate the first item from the original choices array
-    default:
-        $choices[$i]['text'] = $first_choice;
-        $choices[$i]['value'] = '';
-        $i++;
-        break;
-    }
-    
-    foreach($terms as $term) {
-        $choices[$i]['text'] = $term->name;
-        $choices[$i]['value'] = $term->term_id;
-        $i++;
-    }
-    
-    return $choices;
-}
-
-add_action('gform_post_submission', 'ounce_gform_post_submission', 10, 2);
-function ounce_gform_post_submission($entry, $form) {
-    
-    // if no post was created, return
-    if(!$entry['post_id'])
-        return;
-    
-    foreach($form['fields'] as $field){
-        
-        if(!$field['populateTaxonomy'])
-            continue;
-        
-        $taxonomy = $field['populateTaxonomy'];
-        $term_id = $entry[$field['id']];
-    }
-    
-    // if we have a taxonomy and a field id, add term to post
-    if($taxonomy && $term_id)
-        ounce_add_term_to_post($taxonomy, $term_id, $entry['post_id']);
-
-}
-
-// add term from taxonomy to post
-function ounce_add_term_to_post($taxonomy = "categories", $term_id, $post_id) {
-
-    $terms = get_terms($taxonomy, array('hide_empty' => 0));
-    
-    foreach($terms as $term) {
-        
-        if($term->term_id == $term_id)
-            $result = wp_set_object_terms($post_id, (int) $term_id, $taxonomy, false);
-            
-    }
-
-}
-
-/*-------------------------------------------------------------------------------------------*/
-/* Add Custom Post Types to Author Archives Page in WordPress
+/*-------------------------------------------------------------------------------------------
+ * Page-level DocBlock
+ * @package WordPress
+ *-------------------------------------------------------------------------------------------
  *
- *@link http://isabelcastillo.com/add-custom-post-types-to-author-archives-wordpress
-/*-------------------------------------------------------------------------------------------*/
-function custom_post_author_archive($query) {
-    if ($query->is_author)
-        $query->set( 'post_type', array('product') );
-    remove_action( 'pre_get_posts', 'custom_post_author_archive' );
+ * Random Greetings to the user.
+ * 
+ * @access public
+ * @param $variable 
+ * @since 1.0
+ * @subpackage Woocommerce
+ * 
+ * @author: Jamie Brewer ( jamie.brwr@gmail.com )
+ * 
+ */
+function sjm_user_greetings() {
+	/** These are the lyrics to Hello Dolly */
+	$geetings = "Hello
+	Hi
+	Hey
+	How's it going
+	What's up";
+	
+	// Here we split it into lines
+	$geetings = explode( "\n", $geetings );
+	
+	// And then randomly choose a line
+	return wptexturize( $geetings[ mt_rand( 0, count( $geetings ) - 1 ) ] );
 }
-add_action('pre_get_posts', 'custom_post_author_archive');
 
 
+/*-------------------------------------------------------------------------------------------
+ * Page-level DocBlock
+ * @package WordPress
+ *-------------------------------------------------------------------------------------------
+ *
+ * Website Networth
+ *
+ * This function collects the price for all Woocommerce
+ * products. It adds the array of integers together and
+ * and then formats them as USD currency.
+ * 
+ * @access public
+ * @param $variable 
+ * @since 1.0
+ * @subpackage Woocommerce
+ * 
+ * @author: Jamie Brewer ( jamie.brwr@gmail.com )
+ * 
+ */
+function sjm_networth( $before, $after ) {
 
-/* Remove Admin Bar for everyonw except Administrator */
-add_action('after_setup_theme', 'remove_admin_bar');
+	global $wpdb;
 
-function remove_admin_bar() {
-	if (!current_user_can('administrator') && !is_admin()) {
-	  show_admin_bar(false);
+	$totals = $wpdb->get_col( "SELECT meta_value FROM wp_postmeta WHERE meta_key = '_price'");
+	
+	$sum = 0;
+	foreach ($totals as $value) {
+	    $sum += $value;
 	}
-}
-
-/*
-Woocommerce: assign an “author” to a product
-@link	http://wordpress.stackexchange.com/questions/74054/woocommerce-assign-an-author-to-a-product
-*/
-add_action('init', 'wpse_74054_add_author_woocommerce', 999 );
-
-function wpse_74054_add_author_woocommerce() {
-    add_post_type_support( 'product', 'author' );
+	setlocale(LC_MONETARY, 'en_US');
+	echo $before . 'Current Networth' . money_format('%(#10n', $sum) . "\n" . $after;
+	unset($value); // break the reference with the last element
 }
 
 
-// Display 24 products per page. Goes in functions.php
-add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 15;' ), 20 );
+/*-------------------------------------------------------------------------------------------
+ * Page-level DocBlock
+ * @package WordPress
+ *-------------------------------------------------------------------------------------------
+ *
+ * Returns the total number of products on the website.
+ *
+ * @todo Restrict to only published products
+ * 
+ * @access public
+ * @param $variable 
+ * @since 1.0
+ * @subpackage Woocommerce
+ * 
+ * @author: Jamie Brewer ( jamie.brwr@gmail.com )
+ * 
+ */
+function sjm_total_product_posts( $before, $after ){
+	
+	global $wpdb;
+	
+	$numposts = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->posts WHERE post_type = 'product'" );
+	
+	if ( 0 < $numposts )
+		echo $before, $numposts = number_format( $numposts ), $after;
+}
 
 
-/* List all WP Users */
-function contributors() {
-global $wpdb;
-$authors = $wpdb->get_results("SELECT ID, user_nicename from $wpdb->users ORDER BY display_name");
+/*-------------------------------------------------------------------------------------------
+ * Page-level DocBlock
+ * @package WordPress
+ *-------------------------------------------------------------------------------------------
+ *
+ * Count and display the number of products added to the website each day.
+ * 
+ * @access public
+ * @param $variable 
+ * @since 1.0
+ * @subpackage Woocommerce
+ * 
+ * @author: Jamie Brewer ( jamie.brwr@gmail.com )
+ * 
+ */
 
-$counter = 1;
+class TodaysPosts {
 
-echo '<ul class="products">';
-foreach($authors as $author) {
-
-		$first = ($counter % 6 == 0) ? ' first' : '';
-		$last = ($counter % 5 == 0) ? ' last' : '';
-		
-		echo '<li class="product', $first, $last,'">';
-			echo '<a href="'.get_bloginfo('url').'/?author='.$author->ID.'">'.get_avatar($author->ID).'</a>';
-			echo '<a href="'.get_bloginfo('url').'/?author='.$author->ID.'">'.get_the_author_meta('display_name', $author->ID).'</a>';
-		echo '</li>';
-		$counter++;
+	//global $wpdb;
+	
+	public $prop1 = "I'm a class property!";
+	private $args = array (
+				'post_type' => 'product',
+				'post_status' => 'publish',
+				'showposts' => -1,
+				'caller_get_posts'=> 1
+			);
+	
+	public function filter_where_footer( $where = '' ) {
+		global $wpdb;
+		//posts in the last 1 day
+		$where .= " AND post_date > '" . date( 'Y-m-d', strtotime( '-1 days' ) ) . "'";
+		return $where;
 	}
-echo '</ul>';
+	
+	public function __construct(){
+		add_filter( 'posts_where', 'filter_where_footer' );
+	}
+	
 }
 
 
-/* Remove Reviews from bottom of single product */
-add_filter( 'woocommerce_product_tabs', 'sb_woo_remove_reviews_tab', 98);
-function sb_woo_remove_reviews_tab($tabs) {
 
- unset($tabs['reviews']);
 
- return $tabs;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
