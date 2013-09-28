5ca5c9d9f54dc2b7423122540c0c6a6920665080
Jamie Brewer <jamie.brwr@gmail.com>
2013-09-28 16:16:39 -0500

    Page Title Update
    Updated the page title from "About" to "Latest Posts".

--- wp-content/themes/swap-and-shop/author-bio.php
+++ wp-content/themes/swap-and-shop/author-bio.php
@@ -13,7 +13,7 @@
 		<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentythirteen_author_bio_avatar_size', 120 ) ); ?>
 	</div><!-- .author-avatar -->
 	<div class="author-description">
-		<h2 class="author-title"><?php printf( __( 'About %s', 'twentythirteen' ), get_the_author() ); ?></h2>
+		<h2 class="author-title"><?php printf( __( '%s\'s Latest Posts', 'twentythirteen' ), get_the_author() ); ?></h2>
 		<p class="author-bio">
 			<?php the_author_meta( 'description' ); ?>
 			<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
